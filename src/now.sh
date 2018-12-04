fb52adbbbe15744909ab057e5d134b3f17c5eaa5
nju33 <nju33.ki@gmail.com>
2018-12-04 11:09:13 +0900

    fix

--- src/now.sh
+++ src/now.sh
@@ -4,7 +4,7 @@ fn() {
   appname=$1
 
   now -n "$appname" \
-  && sh -c "now ls \"$appname\" | while read line; do if echo \"$line\" | grep \""$appname"-\" > /dev/null; then set $line; echo \"$2\"; break; fi; done;' | xargs -I@ now ln @ \"$appname\"" \
+  && sh -c "now ls \"$appname\" | while read line; do if echo \"$line\" | grep \""$appname"-\" > /dev/null; then set $line; echo \"$2\"; break; fi; done;" | xargs -I@ now ln @ \"$appname\" \
   && now rm "$appname" --safe --yes
 }
 
